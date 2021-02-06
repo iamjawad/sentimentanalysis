1b8be9f76db49cb8c094a5f356c73bfbf6cd6008
Jawad Ahmed <ijawadak@gmail.com>
2021-02-07 02:22:59 +0500

    API Guideline

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,3 +96,15 @@ var app = new Vue({
         }
     }
 });
+
+$(function() {
+    var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+})
